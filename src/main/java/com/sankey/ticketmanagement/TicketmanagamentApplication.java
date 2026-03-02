2d80c902052e65fc94ff24b87f954ccc4aad3aa5
AmanMomin2207 <amanchandsomomin7860@gmail.com>
2026-03-02 21:16:34 +0530

    Enable Method Security

--- src/main/java/com/sankey/ticketmanagement/TicketmanagamentApplication.java
+++ src/main/java/com/sankey/ticketmanagement/TicketmanagamentApplication.java
@@ -2,8 +2,10 @@ package com.sankey.ticketmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class TicketmanagamentApplication {
 
 	public static void main(String[] args) {
